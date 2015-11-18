3bd19ce37e9e4c8b3ba55db29d06d75822957265
臧其龙 <zangqilong@zangqilemacbook.lan>
2015-11-18 14:17:41 +0800

    Initial Commit

---
+++ CustomORM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomORM
+//
+//  Created by 臧其龙 on 15/11/18.
+//  Copyright © 2015年 臧其龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
